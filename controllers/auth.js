edd3ff4eb92469f07081c333d6042991f8526106
MAlngaawy <m.alngaawy@gmail.com>
2023-02-26 08:28:57 +0200

    add signup user to the database

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,29 @@
+const User = require("../models/user");
 exports.signup = (req, res) => {
-  console.log("POST REQ BODY ", req.body);
-  res.json({
-    data: "Hello World From routes folder from controllers",
+  //Access the request data
+  const { name, email, password } = req.body;
+
+  //Check if the user email istajen in the databse
+  User.findOne({ email }).exec((err, user) => {
+    if (user) {
+      // the app will end here -with this return- if the user has exest
+      return res.status(400).json({
+        error: "Email is taken",
+      });
+    } else {
+      let newUser = new User({ email, name, password });
+      newUser.save((err, success) => {
+        if (err) {
+          console.log("SIGNUP ERROR", err);
+          return res.status(400).json({
+            error: err,
+          });
+        }
+
+        res.json({
+          message: "Signup success! Please signin",
+        });
+      });
+    }
   });
 };
