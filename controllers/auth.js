45714562a635507203800b6940c519a5c8eacfbb
MAlngaawy <m.alngaawy@gmail.com>
2023-02-27 04:11:35 +0200

    handle sending email to the user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,66 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const { sendEmailWithNodemailer } = require("../helper/email");
+
+// exports.signup = (req, res) => {
+//   //Access the request data
+//   const { name, email, password } = req.body;
+
+//   //Check if the user email istajen in the databse
+//   User.findOne({ email }).exec((err, user) => {
+//     if (user) {
+//       return res.status(400).json({
+//         error: "Email is taken",
+//       });
+//     } else {
+//       let newUser = new User({ email, name, password });
+//       newUser.save((err, success) => {
+//         if (err) {
+//           console.log("SIGNUP ERROR", err);
+//           return res.status(400).json({
+//             error: err,
+//           });
+//         }
+
+//         res.json({
+//           message: "Signup success! Please signin",
+//         });
+//       });
+//     }
+//   });
+// };
+
 exports.signup = (req, res) => {
-  //Access the request data
   const { name, email, password } = req.body;
 
-  //Check if the user email istajen in the databse
   User.findOne({ email }).exec((err, user) => {
     if (user) {
       return res.status(400).json({
         error: "Email is taken",
       });
     } else {
-      let newUser = new User({ email, name, password });
-      newUser.save((err, success) => {
-        if (err) {
-          console.log("SIGNUP ERROR", err);
-          return res.status(400).json({
-            error: err,
-          });
-        }
+      const token = jwt.sign(
+        { name, email, password },
+        process.env.JWT_ACCOUNT_ACTIVATION,
+        { expiresIn: "10m" }
+      );
 
-        res.json({
-          message: "Signup success! Please signin",
-        });
-      });
+      const emailData = {
+        from: process.env.GMAIL_SENDER_EMAIL, // MAKE SURE THIS EMAIL IS YOUR GMAIL FOR WHICH YOU GENERATED APP PASSWORD
+        to: email, // WHO SHOULD BE RECEIVING THIS EMAIL? IT SHOULD BE THE USER EMAIL (VALID EMAIL ADDRESS) WHO IS TRYING TO SIGNUP
+        subject: "ACCOUNT ACTIVATION LINK",
+        html: `
+                <h1>Please use the following link to activate your account</h1>
+                <p>http://localhost:3000/auth/activate/${token}</p>
+                <hr />
+                <p>This email may contain sensitive information</p>
+                <p>http://localhost:3000</p>
+            `,
+      };
+
+      sendEmailWithNodemailer(req, res, emailData);
     }
   });
 };
